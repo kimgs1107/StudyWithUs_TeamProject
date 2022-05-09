9dd2d7291691f80fd4d1bcb391ca43af594f56a4
hyeryun3 <hryun3@gmail.com>
2022-05-09 21:50:44 +0900

    totaltime 도메인 추가 및 구현 완료

--- src/main/java/st/project/studyWithUs/controller/StudyingController.java
+++ src/main/java/st/project/studyWithUs/controller/StudyingController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import st.project.studyWithUs.argumentresolver.Login;
+import st.project.studyWithUs.domain.User;
 import st.project.studyWithUs.domain.UserTeam;
+import st.project.studyWithUs.interceptor.SessionConst;
 import st.project.studyWithUs.service.StudyingService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -22,14 +29,25 @@ public class StudyingController {
 
     @PostMapping("/updateExist")
     @ResponseBody
-    public void updateExist(@RequestParam("data") boolean data) {
+    public void updateExist(HttpServletRequest request,@RequestParam("data") boolean data, @RequestParam("realTime") int realTime, @RequestParam("totalTime") int totalTime, @Login User user) {
 
-        Long uID = 1l;
         Long tID = 1l;
 
-        UserTeam ut = studyingService.findUserTeam(uID, tID);
+        UserTeam ut = studyingService.findUserTeam(user.getUID(), tID);
         ut.setExist(data);
+        ut.setRealTime(((long)realTime)/100);
+        ut.setTotalTime(((long)totalTime)/100);
         studyingService.save(ut);
+    }
+
+    @PostMapping("/updateTotalTime")
+    @ResponseBody
+    public int updateTotalTime(@Login User user) {
+
+        Long tID = 1l;
+
+        UserTeam ut = studyingService.findUserTeam(user.getUID(),tID);
+        return ut.getTotalTime().intValue();
     }
 
 }
