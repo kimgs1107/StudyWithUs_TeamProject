c3200e8bc1d2b23332e8efc49a9927794af156dd
hyeryun3 <hryun3@gmail.com>
2022-05-09 17:47:07 +0900

    StudyingRepository -> UserTeamRepository로 변경

--- src/main/java/st/project/studyWithUs/repository/UserTeamRepository.java
+++ src/main/java/st/project/studyWithUs/repository/UserTeamRepository.java
@@ -17,4 +17,7 @@ public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
     @Query("select t from Team t , UserTeam ut where ut.user.uID = :uID and ut.team.tID = t.tID")
     List<Team>  findUserTeam(@Param("uID") Long uID);
 
+    @Query("select ut from UserTeam ut where ut.user.uID = :uID and ut.team.tID = :tID")
+    UserTeam findByuIDandtID(@Param("uID") Long uID, @Param("tID") Long tID);
+
 }
--- src/main/java/st/project/studyWithUs/service/StudyingServiceImpl.java
+++ src/main/java/st/project/studyWithUs/service/StudyingServiceImpl.java
@@ -3,22 +3,22 @@ package st.project.studyWithUs.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import st.project.studyWithUs.domain.UserTeam;
-import st.project.studyWithUs.repository.StudyingRepository;
+import st.project.studyWithUs.repository.UserTeamRepository;
 
 @Service
 @RequiredArgsConstructor
 public class StudyingServiceImpl implements StudyingService{
 
-    private final StudyingRepository studyingRepository;
+    private final UserTeamRepository userTeamRepository;
 
     @Override
     public void save(UserTeam ut) {
-        studyingRepository.save(ut);
+        userTeamRepository.save(ut);
     }
 
     @Override
     public UserTeam findUserTeam(Long uID, Long tID) {
-        return studyingRepository.findByuIDandtID(uID,tID);
+        return userTeamRepository.findByuIDandtID(uID,tID);
     }
 
 
