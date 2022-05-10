004d2d37b19dd37d9c1e0025d4395cab8874a738
hyeryun3 <hryun3@gmail.com>
2022-05-10 17:36:23 +0900

    회원가입

---
+++ src/main/java/st/project/studyWithUs/form/UserForm.java
@@ -0,0 +1,24 @@
+package st.project.studyWithUs.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class UserForm {
+
+    @NotEmpty(message = "아이디는 필수 입니다")
+    private String id;
+
+    @NotEmpty(message = "비밀번호는 필수 입니다")
+    private String passWord;
+
+    @NotEmpty(message = "이름은 필수 입니다")
+    private String name;
+
+    @NotEmpty(message = "E-mail은 필수 입니다")
+    private String email;
+
+}
