37af39ce98a26f684b03d838e6dac1bd8295a0c1
hyeryun3 <hryun3@gmail.com>
2022-05-17 23:05:13 +0900

    네이버 캡차(자동입력 방지)

--- src/main/java/st/project/studyWithUs/service/naverService/NaverService.java
+++ src/main/java/st/project/studyWithUs/service/naverService/NaverService.java
@@ -1,151 +1,20 @@
 package st.project.studyWithUs.service.naverService;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.HashMap;
+import java.util.Map;
 
 @Service
-public class NaverService {
+public interface NaverService {
 
-    static String clientId = "quDmxXiN1GypP5C8hzbi"; //애플리케이션 클라이언트 아이디값";
-    static String clientSecret = "2DCTt5z1X0"; //애플리케이션 클라이언트 시크릿값";
+    String getNaverToken(String code);
+    HashMap<String, Object> getUserInfo (String access_Token);
+    void naverLogout(String access_Token);
 
-    public String getNaverToken(String code){
-        String access_Token="";
-        String reqURL = "https://nid.naver.com/oauth2.0/token";
+    String getKey();
+    String receiveImg(String key);
 
-        try{
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-            StringBuilder sb = new StringBuilder();
-            sb.append("grant_type=authorization_code");
-            sb.append("&client_id="+clientId);
-            sb.append("&client_secret="+clientSecret);
-            sb.append("&code=" + code);
-
-            bw.write(sb.toString());
-            bw.flush();
-
-            int responseCode = conn.getResponseCode();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line = "";
-            String result = "";
-
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
-
-            //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
-
-            JsonParser parser = new JsonParser();
-            JsonElement element = parser.parse(result);
-
-            access_Token = element.getAsJsonObject().get("access_token").getAsString();
-
-            br.close();
-            bw.close();
-        }catch (
-                IOException e) {
-            e.printStackTrace();
-        }
-        return access_Token;
-    }
-
-    public HashMap<String, Object> getUserInfo (String access_Token) {
-
-        //    요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
-        HashMap<String, Object> userInfo = new HashMap<>();
-        String reqURL = "https://openapi.naver.com/v1/nid/me";
-        try {
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-
-            //    요청에 필요한 Header에 포함될 내용
-            conn.setRequestProperty("Authorization", "Bearer " + access_Token);
-            conn.setRequestProperty("Content-type","application/x-www-form-urlencoded;charset=utf-8");
-
-            int responseCode = conn.getResponseCode();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-            String line = "";
-            String result = "";
-
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
-
-            System.out.println("========================");
-            System.out.println(result);
-
-            JsonParser parser = new JsonParser();
-            JsonElement element = parser.parse(result);
-
-            JsonObject properties = element.getAsJsonObject().get("response").getAsJsonObject();
-
-
-            String name = properties.getAsJsonObject().get("name").getAsString();
-            String email = properties.getAsJsonObject().get("email").getAsString();
-
-            System.out.println(name);
-            System.out.println(email);
-            userInfo.put("name", name);
-            userInfo.put("email", email);
-
-
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        return userInfo;
-    }
-
-    public void naverLogout(String access_Token) {
-        String reqURL = "https://nid.naver.com/oauth2.0/token";
-        try {
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-            StringBuilder sb = new StringBuilder();
-            sb.append("grant_type=delete");
-            sb.append("&client_id="+clientId);
-            sb.append("&client_secret="+clientSecret);
-            sb.append("&access_token=" + access_Token);
-
-            bw.write(sb.toString());
-            bw.flush();
-
-            int responseCode = conn.getResponseCode();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String result = "";
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
+    String resCap(String key, String value);
 
 }
