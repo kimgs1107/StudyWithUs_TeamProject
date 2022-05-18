da2fb5f4ddd0813537f155cb43eb3c1d102421bf
hyeryun3 <hryun3@gmail.com>
2022-05-18 11:28:08 +0900

    네이버 로그아웃 수정

--- src/main/java/st/project/studyWithUs/service/naverService/NaverServiceImpl.java
+++ src/main/java/st/project/studyWithUs/service/naverService/NaverServiceImpl.java
@@ -121,39 +121,6 @@ public class NaverServiceImpl implements NaverService{
         return userInfo;
     }
 
-    public void naverLogout(String access_Token) {
-        String reqURL = "https://nid.naver.com/oauth2.0/token";
-        try {
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-            StringBuilder sb = new StringBuilder();
-            sb.append("grant_type=delete");
-            sb.append("&client_id="+clientId);
-            sb.append("&client_secret="+clientSecret);
-            sb.append("&access_token=" + access_Token);
-
-            bw.write(sb.toString());
-            bw.flush();
-
-            int responseCode = conn.getResponseCode();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String result = "";
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
     public String getKey(){
         String apiURL = "https://openapi.naver.com/v1/captcha/nkey?code=0";
         Map<String, String> requestHeaders = new HashMap<>();
